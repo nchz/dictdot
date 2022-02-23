cfd025e787bbb7cd37f7fc62177cb502578d7be9
Ignacio Gonzalez <8524584+nchz@users.noreply.github.com>
2022-02-23 19:44:51 -0300

    readme and setup.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="dictdot",
+    version="1.0.0",
+    author="nchz",
+    url="https://github.com/nchz/dictdot",
+    description="Python dict accessible by dot.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    packages=find_packages(),
+)
