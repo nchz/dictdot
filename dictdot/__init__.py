dbb3a10489c439939f381b990b434675d04dc6b9
Ignacio Gonzalez <8524584+nchz@users.noreply.github.com>
2022-02-27 20:15:23 -0300

    recursively convert nested dicts.

--- dictdot/__init__.py
+++ dictdot/__init__.py
@@ -3,3 +3,18 @@ class dictdot(dict):
     __getattr__ = dict.get
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+    def __init__(self, dic, recursive=True):
+        """Convert `dic` to `dictdot` (nested dicts too if `recursive`)."""
+        self.update(**dic)
+        if recursive:
+            for k, v in self.items():
+                self[k] = self._nest(v)
+
+    def _nest(self, value):
+        if type(value) is dict:
+            return self.__class__(value)
+        elif type(value) in [list, tuple]:
+            return [self._nest(v) for v in value]
+        else:
+            return value
