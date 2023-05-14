f3eb9a121fe2b0165f7f5efa142150d3f570be5b
Ignacio Gonzalez <8524584+nchz@users.noreply.github.com>
2023-05-14 18:21:03 -0300

    add `find` function.

--- dictdot/__init__.py
+++ dictdot/__init__.py
@@ -1,8 +1,62 @@
-"""Python dict accessible by dot."""
 import re
 
 
+# Helper functions.
+
+
+def _is_valid_dot_key(s):
+    """
+    Return True if `s` can be used to access a `dictdot` key by dot notation.
+    """
+    return (
+        s
+        and re.sub(r"\w", "", s) == ""
+        and not s[0].isdigit()
+        and s not in dictdot.__dict__
+    )
+
+
+def _build_path(*args):
+    """
+    Concatenate `args` to create a string that can be used to access a `dictdot`
+    (nested) element.
+    """
+    result = ""
+    for x in args:
+        if isinstance(x, str):
+            if _is_valid_dot_key(x):
+                result += f".{x}"
+            else:
+                x = x.replace('"', r"\"")
+                result += f'["{x}"]'
+        else:
+            result += f"[{x}]"
+    return result
+
+
+def _true(*a, **kw):
+    """Just return True."""
+    return True
+
+
+_func = type(_true)
+
+
+# Main class.
+
+
 class dictdot(dict):
+    """
+    Python dict accessible by dot. For example:
+
+        from dictdot import dictdot
+
+        d = dictdot({"foo": 1})
+        d.bar = 1
+        assert d.foo == d["bar"]
+
+    """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for k, v in self.items():
@@ -33,6 +87,60 @@ class dictdot(dict):
         else:
             return value
 
+    @staticmethod
+    def find(it, *, max_depth=0, check_key=_true, check_value=_true, prev=None):
+        """
+        Recursively examine `it` and yield its (nested) paths that contain keys
+        and values that return True when passed as arguments to the functions
+        `check_key` and `check_value`, respectively. The "paths" found by this
+        function are returned as strings, hence they are not meant to be used
+        programatically.
+
+        By default `check_key` and `check_value` always return True.
+
+        If `check_key` (or `check_value`) is an explicit function, then it will
+        be applied to each key (or value).
+
+        If `check_key` (or `check_value`) is not a function, then it will be
+        replaced by a function that checks that the passed argument is equal to
+        the proived `check_key` (or `check_value`). Then, `check_key="3.14"` is
+        a shortcut for `check_key=lambda x: x == "3.14"`.
+        """
+        if prev is None:
+            prev = []
+        if max_depth and len(prev) >= max_depth:
+            return
+        if type(check_key) != _func:
+            _key = check_key
+            check_key = lambda x: x == _key
+        if type(check_value) != _func:
+            _value = check_value
+            check_value = lambda x: x == _value
+
+        if isinstance(it, dict):
+            for k, v in it.items():
+                if check_key(k) and check_value(v):
+                    yield _build_path(*prev, k)
+                yield from dictdot.find(
+                    v,
+                    check_key=check_key,
+                    check_value=check_value,
+                    prev=[*prev, k],
+                    max_depth=max_depth,
+                )
+        elif isinstance(it, (list, tuple)):
+            for i, x in enumerate(it):
+                yield from dictdot.find(
+                    x,
+                    check_key=check_key,
+                    check_value=check_value,
+                    prev=[*prev, i],
+                    max_depth=max_depth,
+                )
+        else:
+            if check_value is not _true and check_value(it):
+                yield _build_path(*prev)
+
     # Methods to handle items.
 
     def __setitem__(self, name, value):
@@ -50,9 +158,7 @@ class dictdot(dict):
         that matches the regex that uses "_"s in `name` as masking characters
         for "." and "-".
         """
-        if name in self.__dict__:
-            return self.__dict__[name]
-        elif name in self:
+        if name in self:
             return self[name]
         else:
             pattern = re.compile("^" + name.replace("_", r"[\.|\-]") + "$")
